00d8c3ed8c0d06562345bf8b17dc84498122f008
lionmoon60 <lionmoon60@yahoo.com>
2017-05-15 05:16:44 +0200

    factorail

---
+++ Recurrsion/factorial/factorial_recurrsion/main.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n);
+
+int main()
+{
+    int n;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    cout << "Factorial of " << n << " = " << factorial(n)<<"\n";
+
+    return 0;
+}
+
+int factorial(int n)
+{
+    //if(n > 1)
+     //   return n * factorial(n - 1);
+    //else
+     //   return 1;
+
+    //base_case that make the recursion stop
+    if(n==1)  return 1;
+    else      return n*factorial(n-1);     //in iterativly way we do factorial of any number as [multiplue one then increment one till reach the number we want it's factoral]
+    //but we will do this in the recurrsion concept going from the number down to one
+
+
+}
