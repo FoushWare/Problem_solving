8237a8075ce70ceb2c5d981a7ff92b81c927b254
lionmoon60 <lionmoon60@yahoo.com>
2017-06-28 08:02:32 +0200

    last update for Blocked CLA :)

--- organiztion_project/functions.c
+++ organiztion_project/functions.c
@@ -82,7 +82,7 @@ void CLA(){
 int num1,num1A,num2,num2B,i=0,cin,sum=0;
 int n1[8],n2[8],si[8],cp[8],cg[8],CIN[8];
 /*initialize the array of number 1   and number 2 cause i want to represent number as an binary in array of zeros and ones*/
-for (i=0;i<7;i++){n1[i]=n2[i]=CIN[8]=0;}
+for (i=0;i<7;i++){n1[i]=n2[i]=CIN[i]=0;}
 
 printf("enter first number \n");
 scanf("%d",&num1A);
@@ -157,36 +157,103 @@ printf("C%d = %d \n",i+1,CIN[7-i]);
 usleep(2000000);
 printf("All sums can be obtained 1 gate delay after the carries are computed\n");
 for(i=0;i<8;i++){
-printf("S%d = %d \n",i,si[i]);
+printf("S%d = %d \n",i,si[7-i]);
 }
 
 }
 void BlockCLA(){
 
+int num1,num1A,num2,num2B,i=0,cin,sum=0,C8=0;
+int n1[16],n2[16],si[16],cp[16],cg[16],CIN[16];
+/*initialize the array of number 1   and number 2 cause i want to represent number as an binary in array of zeros and ones*/
+for (i=0;i<15;i++){n1[i]=n2[i]=CIN[i]=0;}
+
+printf("enter first number \n");
+scanf("%d",&num1A);
+printf("enter second number \n");
+scanf("%d",&num2B);
+
+num1=num1A; //this is only for printing cause num1 will be manipulated and it's value will be changed
+num2=num2B;//this is only for printing cause num2 will be manipulated and it's value will be changed
+
+/*binary representation*/
+i=15;
+do{
+        n1[i]=num1%2;
+        num1=num1/2;
+        n2[i]=num2%2;
+        num2=num2/2;
+   i--;
+}while( (num1 !=0) || (num2 !=0));
+
+usleep(600000);
+printf("\n binary representation\n");
+
+printf("num1 = %d \t",num1A);
+for(i=0;i<16;i++) printf("%d  ",n1[i]);
+
+printf("\nnum2 = %d \t",num2B);
+for(i=0;i<16;i++) printf("%d  ",n2[i]);
 
 
+cin=0;
+//the summation process
+for(i=15;i>=8;i--){
+si[i]=XOR(cin,XOR(n1[i],n2[i]));
+cg[i]=And(n1[i],n2[i]);
+cp[i]=XOR(n1[i],n2[i]);
+
+cin=OR(cg[i],And(cin,cp[i]));
+
+CIN[i]=cin;
+
+}
+C8=CIN[8];
+
+//the summation process second level
+for(i=7;i>=0;i--){
+si[i]=XOR(C8,XOR(n1[i],n2[i]));
+cg[i]=And(n1[i],n2[i]);
+cp[i]=XOR(n1[i],n2[i]);
+
+cin=OR(cg[i],And(cin,cp[i]));
+
+CIN[i]=cin;
+
+}
+
+/*** printing the sum in the form of binary */
+usleep(2000000);
+printf("\nsum: \t\t");
+for(i=0;i<16;i++) {
+        printf("%d  ",si[i]);
+ /******getting the sum by  the weight of the  resulting sum */
+sum=sum+ si[i]*pow(2,15-i);
+}
+usleep(2000000);
+printf("\nsum of %d+%d = %d",num1A,num2B,sum);
+
+printf("\n *****calculate the critical path in this example it will be c16*****/n");
 
 
+usleep(2000000);
+printf("One gate delay for Pi and Gi\n");
+
+for(i=0;i<16;i++){
+printf("p0%d = %d \t \t",i,cp[15-i]);
+printf("G0%d = %d  \n",i,cg[15-i]);
+}
 
 
+printf("Two gate delays in the AND-OR circuit for ci+1 For level 2 \n");
+for(i=0;i<=7;i++){
+printf("C%d = %d \n",i+1,CIN[7-i]);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+usleep(2000000);
+printf("Two gate delays in the AND-OR circuit for ci+1\n");
+for(i=8;i<=15;i++){
+printf("C%d = %d \n",i+1,CIN[15-i]);
+}
 
 }
