2b4b9f756226b2f2be66df23823686a254b520e3
lionmoon60 <lionmoon60@yahoo.com>
2017-07-20 02:52:08 +0200

    this is pointer i wanted to write more code about it but i just wanted to remeber somethings about it .cause i wrote complex things with it in the past .and i will enchance it in the future if i need it

---
+++ C++_course/pointers/Pointers_practice1/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*first section
+   int val=15;
+   int *ptr=&val;
+
+   cout<< val <<endl;
+   cout<< &val <<endl;
+   cout<< *ptr <<endl;
+   cout<< ptr <<endl;
+   cout<< &ptr <<endl;
+  */
+
+/* second section
+ *  the first element in the array has address
+ *  equal to the address of the array
+
+    int arr[5]={1,2,3,4,5};
+    cout<< arr <<endl;
+    cout<< &arr[0] <<endl;
+*/
+
+/* Section three
+ * pointer Artimatics*/
+
+
+
+
+}
