10012a3b1ae8d9cec2804af4e40825814b943e55
lionmoon60 <lionmoon60@yahoo.com>
2017-07-26 23:22:00 +0200

    input and output stream overloading to make streaming for objects rather than built_in type

--- C++_course/OOP/main.cpp
+++ C++_course/OOP/main.cpp
@@ -309,6 +309,49 @@ int main() {
 }
 */
 
+/*input and output stream overloading this will make me enter and out value of type object
+class Distance{
+   private:
+        int feet;
+        int inches;
+   public:
+        Distance(){
+            feet=0;
+            inches=0;
+        }
+   Distance(int f,int i){
+      feet=f;
+      inches=i;
+   }
 
+   friend ostream &operator<<(ostream &output,
+       const Distance &D){
+       output<<" F : "<< D.feet<<" I: "<<D.inches;
+       return output;
 
+   }
 
+   friend istream &operator>>(istream &input,
+
+       Distance &D  ){
+
+       input >> D.feet >> D.inches;
+       return input;
+   }
+
+};
+
+int main(){
+
+   Distance D1(11,10),D2(5,11),D3;
+
+   cout<<"Enter the value of object"<<endl;
+   cin>>D3;
+   cout<<"first Distance : "<<D1<<endl;
+   cout<<"second Distance : "<<D2<<endl;
+   cout<<"third Distance : "<<D3<<endl;
+
+   return 0;
+
+}
+*/
