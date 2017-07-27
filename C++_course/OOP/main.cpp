90ab167964858307b256a44c5c3816524ca189d8
lionmoon60 <lionmoon60@yahoo.com>
2017-07-27 04:22:15 +0200

    operator overloading ++ and --

--- C++_course/OOP/main.cpp
+++ C++_course/OOP/main.cpp
@@ -355,3 +355,77 @@ int main(){
 
 }
 */
+
+/*operator overloading increment prefix and postfix
+
+class Time{
+   private:
+        int hours;
+        int minutes;
+   public:
+        //default constractor
+        Time(){
+            hours=0;
+            minutes=0;
+        }
+        Time(int h,int m){
+            hours=h;
+            minutes=m;
+        }
+
+
+        void displayTime(){
+            cout<<" h: "<<hours<<" m: "<<minutes <<endl;
+
+        }
+
+       Time operator ++(){
+            ++minutes;
+           if(minutes >= 60)
+           {
+               ++hours;
+               minutes-=60;
+
+           }
+           return Time(hours,minutes);
+       }
+
+       Time operator ++(int){
+         Time T(hours,minutes);
+
+          ++minutes;
+            if(minutes >= 60)
+           {
+               ++hours;
+               minutes-=60;
+
+           }
+           return T;
+
+       }
+};
+
+int main() {
+   Time T1(11, 59), T2(10,40);
+
+   ++T1;                    // increment T1
+   T1.displayTime();        // display T1
+   ++T1;                    // increment T1 again
+   T1.displayTime();        // display T1
+
+   T2++;                    // increment T2
+   T2.displayTime();        // display T2
+   T2++;                    // increment T2 again
+   T2.displayTime();        // display T2
+   return 0;
+}
+
+*/
+
+
+
+
+
+
+
+
