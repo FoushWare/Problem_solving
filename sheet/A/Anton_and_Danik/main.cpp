25a008f3d6180729be0d8ad203fc59ba6caafa8e
lionmoon60 <lionmoon60@yahoo.com>
2017-08-01 05:31:35 +0200

    second problem in the sheet category A

---
+++ sheet/A/Anton_and_Danik/main.cpp
@@ -0,0 +1,75 @@
+
+/*************************************************************************
+    Year:2017                                                         *
+    month: 8                                                      *
+    DAY:1                                                           *
+    DATE:8/1/2017                                                         *
+   Author: Ahmed Fouad (Foush)                                          *
+*************************************************************************/
+#include <iostream>
+#include<vector>
+#include<string>
+#include<assert.h>
+
+std::vector<char> s;
+std::vector<char>::iterator it;
+
+int Danik=0,Anton=0,Friendship=0,gameNum=0;
+std::string ResultGame;
+
+
+int main()
+{
+   std::cin>>gameNum;
+
+   assert(gameNum >=1 && gameNum <= 100000);
+
+  std::cin>>ResultGame;
+
+ for(int i=0;i<int(ResultGame.length());i++){
+
+  s.push_back(ResultGame[i]);
+ }
+
+
+  for(it=s.begin();it !=s.end();it++){
+      if(*it == 'A') ++Anton;
+      else if(*it == 'D') ++Danik;
+      else{}
+  }
+
+
+ if(Danik == Anton) std::cout<<"Friendship";
+ else if(Danik > Anton) std::cout<<"Danik";
+ else{
+     std::cout<<"Anton";
+
+}
+
+
+
+
+
+
+    return 0;
+}
+
+/* the editorial code
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int n; cin >> n;
+    string s; cin >> s;
+    int k_a = 0, k_d = 0;
+    for (int i = 0; i < n; i++)
+        if (s[i] == 'A') k_a++; else k_d++;
+    if (k_a > k_d) cout << "Anton" << endl;
+    if (k_a < k_d) cout << "Danik" << endl;
+    if (k_a == k_d) cout << "Friendship" << endl;
+    return 0;
+}
+*/
