5dfa3dd5af1ff8d7b2338ac17cd8229c33a647d9
lionmoon60 <lionmoon60@yahoo.com>
2017-08-20 17:48:31 +0200

    i got WA for the first submition cause i have to check the constraint i should check if the input is outside of int range of equal to it and i have to do opertion involve plus and multiply so i have to use long long int not int

---
+++ sheet/A/Free_ice_cream/main.cpp
@@ -0,0 +1,64 @@
+
+/*************************************************************************
+    Year:2017                                                         *
+    month: 8                                                      *
+    DAY:20                                                           *
+    DATE:8/20/2017                                                         *
+   Author: Ahmed Fouad (Foush)
+
+*************************************************************************/
+
+#include <bits/stdc++.h>
+
+//macro//
+#define maX(a,b)                     ( (a) > (b) ? (a) : (b))
+#define miN(a,b)                     ( (a) < (b) ? (a) : (b))
+#define ll long long int
+#define ld long double
+#define sz(a) int((a).size())
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define present(c,x) ((c).find(x) != (c).end())
+#define tr(c,i) for(typeof((c).begin() i = (c).begin(); i != (c).end(); i++)
+#define mp make_pair
+//typedef//
+typedef std::vector<int> vi;
+typedef std::vector<vi> vvi;
+typedef std::pair<int,int> ii;
+typedef std::vector<int>::iterator it;
+typedef std::vector<std::string> vs;
+typedef std::vector<char> vc;
+int main()
+{
+std::ios_base::sync_with_stdio(false);
+//variables
+int k_kids=0,k_n=0,k_packs=0,k_num=0;
+long long int  k_total=0;
+char k_sign=' ';
+
+std::cin>>k_n>>k_packs;
+k_total=k_packs;
+vc v1;//sign
+vi v2;//packs
+
+
+for(int i=0;i<k_n;i++){
+    std::cin>>k_sign;
+    v1.pb(k_sign);
+    std::cin>>k_num;
+    v2.pb(k_num);
+}
+
+for(int i=0;i<k_n;i++){
+    if(v1[i]=='+'){
+        k_total += v2[i];
+    }
+    if(v1[i]=='-'){
+        k_total -= v2[i];
+        if(k_total <0){++k_kids; k_total+=v2[i];}
+    }
+}
+
+std::cout<<k_total<<' '<<k_kids;
+    return 0;
+}
